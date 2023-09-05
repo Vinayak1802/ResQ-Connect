1a9aa2102dd2430001e49e80881cf6dd5a9e62e6
Vinayak Jaiswal <96956957+Vinayak1802@users.noreply.github.com>
2023-09-05 13:39:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,36 @@
 const chatbotButton = document.getElementById('chatbot-button');
 const chatbotContainer = document.getElementById('chatbot');
 
+function incrementManMadeCounter() {
+  let manMadeDeaths = 0;
+  const manMadeCounter = document.getElementById('man-made-counter');
+
+  const interval = setInterval(() => {
+      manMadeCounter.innerText = manMadeDeaths++;
+      if (manMadeDeaths > 3100) {
+          clearInterval(interval);
+      }
+  }, 1);
+}
+
+function incrementNaturalCounter() {
+  let naturalDeaths = 0;
+  const naturalCounter = document.getElementById('natural-counter');
+
+  const interval = setInterval(() => {
+      naturalCounter.innerText = naturalDeaths++;
+      if (naturalDeaths > 2400) {
+          clearInterval(interval);
+      }
+  }, 0);
+}
+
+// Event listener to start counters when the page loads
+window.addEventListener('load', () => {
+  incrementManMadeCounter();
+  incrementNaturalCounter();
+}); 
+
 function toggleChatbot() {
   chatbotContainer.classList.toggle('hidden');
 }
